bfdebc9bab0ef6008ec07c9a1dbe6701889aa727
VenkatKadiveti <venkatkadiveti248@gmail.com>
2023-12-17 20:32:09 +0530

    V01-Inception

---
+++ App.js
@@ -0,0 +1,33 @@
+const h1Ele = React.createElement('h1',
+    {
+        className: 'helloWorld',
+        id: 'headeing',
+        name: 'heading'
+    },
+    'Hello world from React!'
+);
+
+const rootEle = ReactDOM.createRoot(document.getElementById('root'));
+
+// rootEle.render(h1Ele);
+
+const parent = React.createElement(
+    'div',
+    { id: 'parent' },
+    React.createElement(
+        'div',
+        { id: 'child' },
+        [
+            React.createElement(
+                'h1',
+                { id: 'h1' },
+                'Heading'),
+            React.createElement(
+                'h2',
+                { id: 'h2' },
+                'Sub-Heading')
+        ]
+    )
+)
+
+rootEle.render(parent)
